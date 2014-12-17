ff995639d6f645aa31aaada929fde30d771ab47e
Da_Blitz <code@pocketnix.org>
2014-12-17 22:21:37 +1100

    Make boardname reflect hardware name

--- stmhal/boards/CRAZYFLIE2/mpconfigboard.h
+++ stmhal/boards/CRAZYFLIE2/mpconfigboard.h
@@ -1,6 +1,6 @@
 #define CRZYFLYV2_1
 
-#define MICROPY_HW_BOARD_NAME       "Crazyflie_v2"
+#define MICROPY_HW_BOARD_NAME       "Crazyflie 2.0 Rev.C"
 #define MICROPY_HW_MCU_NAME         "STM32F405RG"
 #define MICROPY_PY_SYS_PLATFORM     "crazyflie2"
 
@@ -9,13 +9,13 @@
 #define MICROPY_HW_HAS_MMA7660      (0)
 #define MICROPY_HW_HAS_LIS3DSH      (0)
 #define MICROPY_HW_HAS_LCD          (0)
-#define MICROPY_HW_ENABLE_RNG       (1)
-#define MICROPY_HW_ENABLE_RTC       (1)
-#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_RNG       (0)
+#define MICROPY_HW_ENABLE_RTC       (0)
+#define MICROPY_HW_ENABLE_TIMER     (0)
 #define MICROPY_HW_ENABLE_SERVO     (0)
-#define MICROPY_HW_ENABLE_DAC       (1)
-#define MICROPY_HW_ENABLE_I2C1      (1)
-#define MICROPY_HW_ENABLE_SPI1      (1)
+#define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_I2C1      (0)
+#define MICROPY_HW_ENABLE_SPI1      (0)
 #define MICROPY_HW_ENABLE_SPI3      (0)
 #define MICROPY_HW_ENABLE_CAN       (0)
 
@@ -25,5 +25,5 @@
 #define MICROPY_HW_LED3             (pin_C2) // green (right)
 #define MICROPY_HW_LED4             (pin_C3) // red (right)
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
-#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
-#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRL = pin->pin_mask)
